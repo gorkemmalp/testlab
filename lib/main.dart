5046ef9f34638a4eca495129a1cb07a6d9b854e1
gorkemmalp <gorkem.alp.ga@gmail.com>
2021-03-21 02:03:44 +0300

    4_commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool selected = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,192 +25,172 @@ class _HomeState extends State<Home> {
         body: CustomScrollView(
           slivers: <Widget>[
             SliverAppBar(
-              backgroundColor: Colors.blue,
-              expandedHeight: 250,
+              backgroundColor: Colors.black,
+              expandedHeight: 180,
+              floating: true,
               title: Center(
                   child: Text(
                 "T E S T L A B",
                 style: TextStyle(fontSize: 40, color: Colors.white),
               )),
             ),
-            SliverGrid.count(
-              crossAxisCount: 1,
-              childAspectRatio: 1 / 1.4,
-              children: [
-                Container(
-                  color: Colors.yellow,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.green,
-                        width: 200,
-                        height: 60,
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Giris Yap",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 40),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 200,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Kullanıcı Adı",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 200,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Sifre",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 90,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Giris",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: Colors.red,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.green,
-                        width: 200,
-                        height: 60,
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Giris Yap",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 40),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 90,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Kullanıcı Adı",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 90,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Sifre",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        color: Colors.green,
-                        width: 90,
-                        height: 70,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(borderRadius: BorderRadius.circular(20)),
-                            Center(
-                                child: Text(
-                              "Giris",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )),
-                            MaterialButton(onPressed: () {}),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
                 (BuildContext context, int index) {
                   return Card(
-                    child: Container(
-                      color: Colors.blue[100 * (index % 9 + 1)],
-                      height: 250,
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Item $index",
-                        style: TextStyle(fontSize: 30),
-                      ),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            image: DecorationImage(
+                              image: AssetImage("assets/1_background.jpg"),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          height: 550,
+                          width: MediaQuery.of(context).size.width,
+                          alignment: Alignment.center,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                alignment: Alignment.center,
+                                margin: EdgeInsets.only(bottom: 15),
+                                height: 302,
+                                child: Text(
+                                  "Giriş Yap\n\nTest Çözmeye\n\nBaşla!",
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.white,
+                                      shadows: <Shadow>[
+                                        Shadow(
+                                          offset: Offset(1.0, 1.0),
+                                          blurRadius: 3.0,
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                        ),
+                                        Shadow(
+                                          offset: Offset(10.0, 10.0),
+                                          blurRadius: 8.0,
+                                          color: Color.fromARGB(125, 0, 0, 255),
+                                        ),
+                                      ]),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Container(
+                                height: 175.0,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/1_drop_down_arrow.png"),
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          color: Colors.black,
+                          height: 890,
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(height: 100),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 100,
+                                margin: EdgeInsets.all(40),
+                                child: Text("GİRİŞ YAP",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 60),
+                                    textAlign: TextAlign.center),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20)),
+                                width: MediaQuery.of(context).size.width,
+                                height: 60,
+                                margin: EdgeInsets.fromLTRB(
+                                    30, 20, 30, 20), //L,T,R,B
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: '    Kullanıcı Adı',
+                                      hintStyle: TextStyle(
+                                          color: Colors.black54, fontSize: 25)),
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20)),
+                                width: MediaQuery.of(context).size.width,
+                                height: 60,
+                                margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: '    Şifre',
+                                      hintStyle: TextStyle(
+                                          color: Colors.black54, fontSize: 25)),
+                                ),
+                              ),
+                              Container(
+                                height: MediaQuery.of(context).size.width / 6,
+                                margin: EdgeInsets.all(40), //L,T,R,B
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: 10), //L,T,R,B
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      width:
+                                          MediaQuery.of(context).size.width / 3,
+                                      height:
+                                          MediaQuery.of(context).size.height,
+                                      child: MaterialButton(
+                                        onPressed: null,
+                                        child: Text("GOOGLE",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 20)),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: 10), //L,T,R,B
+                                      decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      width:
+                                          MediaQuery.of(context).size.width / 3,
+                                      height:
+                                          MediaQuery.of(context).size.height,
+                                      child: MaterialButton(
+                                        onPressed: null,
+                                        child: Text("GİRİŞ",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20)),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 },
