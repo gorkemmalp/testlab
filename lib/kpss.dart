6c1b322c6c97703df9c34b232d4955109b91d89c
gorkemmalp <gorkem.alp.ga@gmail.com>
2021-04-06 18:20:28 +0300

    9_commit

--- lib/kpss.dart
+++ lib/kpss.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class KpssView extends StatefulWidget {
   @override
@@ -7,56 +8,44 @@ class KpssView extends StatefulWidget {
 
 class _KpssViewState extends State<KpssView> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Century'),
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.black,
-              expandedHeight: 200,
-              floating: true,
-              title: Center(
-                  child: Text(
-                "K P S S",
-                style: TextStyle(fontSize: 40, color: Colors.white),
-              )),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                color: Colors.black,
-                width: MediaQuery.of(context).size.width,
-                child: GridView.count(crossAxisCount: 2, children: [
-                  Center(
-                    child: Container(
-                      color: Colors.red,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ),
-                  Center(
-                    child: Container(
-                      color: Colors.green,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ),
-                ]),
-                /*child: Column(children: [
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.black,
+                  Colors.blueGrey,
+                  Colors.black,
+                  Colors.blueGrey,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.15, 0.4, 075, 0.9]),
+          ),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                expandedHeight: 230,
+                floating: false,
+                title: Center(
+                    child: Text(
+                  "K P S S",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                )),
+              ),
+              SliverGrid.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 60,
+                crossAxisCount: 2,
+                children: <Widget>[
                   Container(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    height: MediaQuery.of(context).size.width / 1.5,
-                    margin: EdgeInsets.only(bottom: 35),
                     child: MaterialButton(
                       onPressed: () {
-                        //Navigator.pushNamed(context, '/kpss');
+                        Navigator.pushNamed(context, '/');
                       },
                       child: Stack(
                         children: <Widget>[
@@ -68,188 +57,97 @@ class _KpssViewState extends State<KpssView> {
                     ),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    height: MediaQuery.of(context).size.width / 1.5,
-                    margin: EdgeInsets.symmetric(vertical: 35),
                     child: MaterialButton(
                       onPressed: () {
-                        //Navigator.pushNamed(context, '/tyt');
+                        Navigator.pushNamed(context, '/');
                       },
                       child: Stack(
                         children: <Widget>[
                           Image(
-                              image: AssetImage("assets/Lessons/Kimya.png"),
+                              image: AssetImage("assets/Lessons/Turkce.png"),
                               fit: BoxFit.contain),
                         ],
                       ),
                     ),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    height: MediaQuery.of(context).size.width / 1.5,
-                    margin: EdgeInsets.symmetric(vertical: 35),
                     child: MaterialButton(
                       onPressed: () {
-                        //Navigator.pushNamed(context, '/ayt');
+                        Navigator.pushNamed(context, '/');
                       },
                       child: Stack(
                         children: <Widget>[
                           Image(
-                              image: AssetImage("assets/2_AYT.png"),
+                              image: AssetImage("assets/Lessons/Geometri.png"),
                               fit: BoxFit.contain),
                         ],
                       ),
                     ),
                   ),
-                ]),*/
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*import 'package:flutter/material.dart';
-//import 'package:testlab/main.dart';
-
-class KpssView extends StatefulWidget {
-  @override
-  _KpssViewState createState() => _KpssViewState();
-}
-
-class _KpssViewState extends State<KpssView> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'Century'),
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.black,
-              expandedHeight: 200,
-              floating: true,
-              title: Center(
-                  child: Text(
-                "K P S S",
-                style: TextStyle(fontSize: 40, color: Colors.white),
-              )),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            image: DecorationImage(
-                              image: AssetImage("assets/1_background.jpg"),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          height: 432,
-                          width: MediaQuery.of(context).size.width,
-                          alignment: Alignment.center,
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                width: MediaQuery.of(context).size.width,
-                                alignment: Alignment.center,
-                                margin: EdgeInsets.only(bottom: 15),
-                                height: 302,
-                                child: Text(
-                                  "Giriş Yap\n\nTest Çözmeye\n\nBaşla!",
-                                  style: TextStyle(
-                                      fontSize: 50,
-                                      color: Colors.white,
-                                      shadows: <Shadow>[
-                                        Shadow(
-                                          offset: Offset(1.0, 1.0),
-                                          blurRadius: 3.0,
-                                          color: Color.fromARGB(255, 0, 0, 0),
-                                        ),
-                                        Shadow(
-                                          offset: Offset(10.0, 10.0),
-                                          blurRadius: 8.0,
-                                          color: Color.fromARGB(125, 0, 0, 255),
-                                        ),
-                                      ]),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Container(
-                                height: 80.0,
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        "assets/1_drop_down_arrow.png"),
-                                    fit: BoxFit.contain,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          color: Colors.black,
-                          height: 88,
-                          width: MediaQuery.of(context).size.width,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.redAccent,
-                                borderRadius: BorderRadius.circular(25)),
-                            margin:
-                                EdgeInsets.fromLTRB(25, 20, 25, 20), //L,T,R,B
-                            child: FlatButton(
-                                onPressed: () {},
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.only(left: 6),
-                                      alignment: Alignment.center,
-                                      child: Text("G",
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 40)),
-                                    ),
-                                    SizedBox(width: 30),
-                                    Container(
-                                      margin: EdgeInsets.only(right: 6),
-                                      child: Text("Google İle Giriş Yap",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 25),
-                                          textAlign: TextAlign.center),
-                                    )
-                                  ],
-                                )),
-                          ),
-                        )
-                      ],
+                  Container(
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image:
+                                  AssetImage("assets/Lessons/Vatandaslik.png"),
+                              fit: BoxFit.contain),
+                        ],
+                      ),
                     ),
-                  );
-                },
-                childCount: 1,
+                  ),
+                  Container(
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage("assets/Lessons/Cografya.png"),
+                              fit: BoxFit.contain),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage("assets/Lessons/Tarih.png"),
+                              fit: BoxFit.contain),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage(
+                                  "assets/Lessons/Guncel_Bilgiler.png"),
+                              fit: BoxFit.contain),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-*/
--- lib/main.dart
+++ lib/main.dart
@@ -27,102 +27,116 @@ class _HomeState extends State<Home> {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Century'),
       home: Scaffold(
-        body: CustomScrollView(slivers: <Widget>[
-          SliverAppBar(
-            backgroundColor: Colors.black,
-            expandedHeight: 250,
-            floating: true,
-            title: Center(
-                child: Text("S I N A V  S E Ç İ M İ",
-                    style: TextStyle(color: Colors.white, fontSize: 37))),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.black,
+                  Colors.blueGrey,
+                  Colors.black,
+                  Colors.blueGrey,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.15, 0.4, 075, 0.9]),
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              color: Colors.black,
-              width: MediaQuery.of(context).size.width,
-              child: Column(children: [
-                Container(
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  height: MediaQuery.of(context).size.width / 1.5,
-                  margin: EdgeInsets.only(bottom: 35),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/kpss');
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Image(
-                            image: AssetImage("assets/2_KPSS.png"),
-                            fit: BoxFit.contain),
-                        Container(
-                            margin: EdgeInsets.only(bottom: 30),
-                            alignment: Alignment.bottomCenter,
-                            child: Text(
-                              "KPSS",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 45),
-                              textAlign: TextAlign.center,
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  height: MediaQuery.of(context).size.width / 1.5,
-                  margin: EdgeInsets.symmetric(vertical: 35),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/tyt');
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Image(
-                            image: AssetImage("assets/2_TYT.png"),
-                            fit: BoxFit.contain),
-                        Container(
-                            margin: EdgeInsets.only(bottom: 30),
-                            alignment: Alignment.bottomCenter,
-                            child: Text(
-                              "TYT",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 45),
-                              textAlign: TextAlign.center,
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  height: MediaQuery.of(context).size.width / 1.5,
-                  margin: EdgeInsets.symmetric(vertical: 35),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/ayt');
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Image(
-                            image: AssetImage("assets/2_AYT.png"),
-                            fit: BoxFit.contain),
-                        Container(
-                            margin: EdgeInsets.only(bottom: 30),
-                            alignment: Alignment.bottomCenter,
-                            child: Text(
-                              "AYT",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 45),
-                              textAlign: TextAlign.center,
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
-              ]),
+          child: CustomScrollView(slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.transparent,
+              expandedHeight: 230,
+              floating: false,
+              title: Center(
+                  child: Text("S I N A V  S E Ç İ M İ",
+                      style: TextStyle(color: Colors.white, fontSize: 37))),
             ),
-          ),
-        ]),
+            SliverToBoxAdapter(
+              child: Container(
+                color: Colors.transparent,
+                width: MediaQuery.of(context).size.width,
+                child: Column(children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    height: MediaQuery.of(context).size.width / 1.5,
+                    margin: EdgeInsets.only(bottom: 35),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/kpss');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage("assets/2_KPSS.png"),
+                              fit: BoxFit.contain),
+                          Container(
+                              margin: EdgeInsets.only(bottom: 30),
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                "KPSS",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 45),
+                                textAlign: TextAlign.center,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    height: MediaQuery.of(context).size.width / 1.5,
+                    margin: EdgeInsets.symmetric(vertical: 35),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/tyt');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage("assets/2_TYT.png"),
+                              fit: BoxFit.contain),
+                          Container(
+                              margin: EdgeInsets.only(bottom: 30),
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                "TYT",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 45),
+                                textAlign: TextAlign.center,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    height: MediaQuery.of(context).size.width / 1.5,
+                    margin: EdgeInsets.symmetric(vertical: 35),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/ayt');
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Image(
+                              image: AssetImage("assets/2_AYT.png"),
+                              fit: BoxFit.contain),
+                          Container(
+                              margin: EdgeInsets.only(bottom: 30),
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                "AYT",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 45),
+                                textAlign: TextAlign.center,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
